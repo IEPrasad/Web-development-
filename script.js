06d9fd4cc6df86f28c618d568857b01662906044
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-01-31 23:49:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,182 +1,20 @@
-let runningTotal = 0;
-let buffer = "0";
-let previousOperator;
+const jsTense = document.querySelector('.tense-name');
 
-const screen = document.querySelector('.screen');
+function display () {
 
-function buttonClick(value){
-  if(isNaN(value)){
-    handleSymbol(value);
-  }else{
-    handleNumber(value);
-  }
-  screen.innerText = buffer;
-}
-
-function handleSymbol(symbol){
-  switch(symbol){
-    case 'C':
-      buffer = '0';
-      runningTotal = 0;
-      break;
-    case '=':
-      if(previousOperator === null){
-        return;
-      }
-      flushOperation(parseInt(buffer));
-      previousOperator = null;
-      buffer = runningTotal;
-      runningTotal = 0;
-      break;
-    case '←':
-      if(buffer.length ===1){
-        buffer = '0';
-      }else{
-        buffer = buffer.substring(0, buffer.
-          length - 1);
-      }
-      break;
-    case '+':
-    case '−':
-    case '×':
-    case '÷':
-      handleMath(symbol);
-      break;
+  if (jsTense.innerText === 'Present Tense #1') {
+    jsTense.innerHTML = `
+      <div>
+        this is the text
+      </div>
+    `;
+    jsTense.classList.add('js-tense');
+    console.log(jsTense.classList.contains('js-tense'));
+  } else {
+    jsTense.innerHTML = `
+     Present Tense #1
+    `;
+    jsTense.classList.remove('js-tense');
+    console.log(jsTense.classList.contains('js-tense'));
   }
 }
-
-function handleMath(symbol){
-  if(buffer === '0'){
-    return;
-  }
-
-  const intBuffer = parseInt(buffer);
-
-  if(runningTotal === 0){
-    runningTotal = intBuffer;
-  }else{
-    flushOperation(intBuffer);
-  }
-  previousOperator = symbol;
-  buffer = '0';
-}
-
-function flushOperation(intBuffer){
-  if(previousOperator === '+'){
-    runningTotal += intBuffer;
-  }else if(previousOperator === '−'){
-    runningTotal -=intBuffer;
-  }else if(previousOperator === '×'){
-    runningTotal *= intBuffer;
-  }else if(previousOperator === '÷'){
-    runningTotal /= intBuffer;
-  }
-}
-
-function handleNumber(numberString){
-  if(buffer === "0"){
-    buffer = numberString;
-  }else{
-    buffer += numberString;
-  }
-}
-
-function init(){
-  document.querySelector('.calc-buttons').
-  addEventListener('click', function(event){
-    buttonClick(event.target.innerText);     
-  })
-}
-
-init();
-
-  // let runningTotal = 0;
-  // let buffer = "0";
-  // let previousOperator;
-
-  // const screen = document.querySelector('.screen');
-
-  // function buttonClick(value) {
-  //   if (isNaN(value)) {
-  //     handleSymbol(value);
-  //   } else {
-  //     handleNumber(value);
-  //   }
-  //   screen.innerText = buffer;
-  // }
-
-  // function handleSymbol(symbol) {
-  //   switch (symbol) {
-  //     case 'C':
-  //       buffer = '0';
-  //       runningTotal = 0;
-  //       break;
-  //     case '=':
-  //       if (previousOperator === null) {
-  //         return;
-  //       }
-  //       flushOperation(parseInt(buffer));
-  //       previousOperator = null;
-  //       buffer = runningTotal.toString();
-  //       runningTotal = 0;
-  //       break;
-  //     case '←':
-  //       if (buffer.length === 1) {
-  //         buffer = '0';
-  //       } else {
-  //         buffer = buffer.substring(0, buffer.length - 1);
-  //       }
-  //       break;
-  //     case '+':
-  //     case '−':
-  //     case '×':
-  //     case '÷':
-  //       handleMath(symbol);
-  //       break;
-  //   }
-  // }
-
-  // function handleMath(symbol) {
-  //   if (buffer === '0') {
-  //     return;
-  //   }
-
-  //   const intBuffer = parseInt(buffer);
-
-  //   if (runningTotal === 0) {
-  //     runningTotal = intBuffer;
-  //   } else {
-  //     flushOperation(intBuffer);
-  //   }
-  //   previousOperator = symbol;
-  //   buffer = '0';
-  // }
-
-  // function flushOperation(intBuffer) {
-  //   if (previousOperator === '+') {
-  //     runningTotal += intBuffer;
-  //   } else if (previousOperator === '−') {
-  //     runningTotal -= intBuffer;
-  //   } else if (previousOperator === '×') {
-  //     runningTotal *= intBuffer;
-  //   } else if (previousOperator === '÷') {
-  //     runningTotal /= intBuffer;
-  //   }
-  // }
-
-  // function handleNumber(numberString) {
-  //   if (buffer === '0') {
-  //     buffer = numberString;
-  //   } else {
-  //     buffer += numberString;
-  //   }
-  // }
-
-  // function init() {
-  //   document.querySelector('.calc-buttons').addEventListener('click', function (event) {
-  //     buttonClick(event.target.innerText);
-  //   });
-  // }
-
-  // init();
-
